8f26eed80cc6e240bd5dab5d17b929562284d357
Annie Zhou <19739773+AnnieZhou08@users.noreply.github.com>
2020-03-26 10:30:39 -0700

    Update list_popular_listings.py

--- slack/queries/list_popular_listings.py
+++ slack/queries/list_popular_listings.py
@@ -9,15 +9,12 @@ def get_pop_listings(numResult = 20):
 	res = '*Listings and Popularity Score:* \n'
 	with connection:
 		cur = connection.cursor()
-		cur.execute("SELECT * FROM PopularListing")
+		cur.execute("SELECT * FROM PopularListing ORDER BY pop_score LIMIT {}".format(numResult))
 		result = cur.fetchall()
-		count = 0
 		for lsting in result:
 			listing_id = lsting[0].strip()
 			pop_score = lsting[1].strip()
 			res += listing_id + ', ' + pop_score + '\n'
-			count += 1
-			if count >= 20: break
 
 		print(res)
 
